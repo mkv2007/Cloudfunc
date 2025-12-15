d97e0c4c5d2b51619b232f089087980c7eacfa6b
Kowndinya <kowndinyamarepalli2007@gmail.com>
2025-12-15 11:35:46 +0530

    Implement gateway service with JWT verification

--- CloudFunc/gateway/gateway.js
+++ CloudFunc/gateway/gateway.js
@@ -1,1 +1,26 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+function verifyJWT(req, res, next) {
+  console.log("JWT verification placeholder");
+  next();
+}
+
+app.post("/invoke", verifyJWT, (req, res) => {
+  console.log("Incoming request data:", req.body);
+  res.status(200).json({
+    message: "gateway request received"
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Gateway Service is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Gateway Service running on port ${PORT}`);
+});
 
